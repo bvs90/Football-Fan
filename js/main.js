150edbacc0e0e7094e9dc4ecd55d691df1e1ca48
Ben Stone <benjamin.v.stone@gmail.com>
2014-02-10 13:30:24 -0800

    Added getForm function to retrieve team form data from API

--- js/main.js
+++ js/main.js
@@ -2,16 +2,30 @@
 
 var initialise = function(teamName){
   getTopScorers(teamName);
+  //checkJSON()
+  getForm(teamName);
 }
 
+//utility to chekc team names are correct. 
+// var checkJSON = function() {
+//   $.ajax({
+//     url: "http://api.statsfc.com/teams.json?key=SBCwkOLa9b8lmePuTjFIoFmFkdo9cvtAPrhxlA6k&competition&competition=premier-league&year=2013/2014", // teams
+//     url: "http://api.statsfc.com/form.json?key=SBCwkOLa9b8lmePuTjFIoFmFkdo9cvtAPrhxlA6k&competition=premier-league&year=2013/2014", // form  
+//     dataType: "jsonp",
+//     success: function(data){
+//       console.log(data);  
+//     },
+//     error: function(err){
+//       throw err;
+//     } 
+//   })  
+// }
 
 
 // get top scorers 
-
-
 var getTopScorers = function(teamName){
   $.ajax({
-    url: "http://api.statsfc.com/top-scorers.json?key=free&competition=premier-league&team=" + teamName + "&year=2013/2014",    
+    url: "http://api.statsfc.com/top-scorers.json?key=SBCwkOLa9b8lmePuTjFIoFmFkdo9cvtAPrhxlA6k&competition=premier-league&team=" + teamName + "&year=2013/2014",    
     dataType: "jsonp",
     success: function(data){
       // renderScorers(data);
@@ -27,6 +41,50 @@ var getTopScorers = function(teamName){
     } 
   })
 };
+
+// render top scores 
+
+
+// get league table 
+
+
+// render league table 
+
+
+// get results 
+
+// render results 
+
+
+// get form 
+var getForm = function(teamName){
+  $.ajax({
+    url: "http://api.statsfc.com/form.json?key=SBCwkOLa9b8lmePuTjFIoFmFkdo9cvtAPrhxlA6k&competition=premier-league&year=2013/2014",    
+    dataType: "jsonp",
+    success: function(data){
+      $list = $('<ul></ul>');
+      for(var i = 0; i < data.length; i++){
+        if(teamName === data[i]['teampath']) {
+          $form = $('<li>' + data[i].form + '</li>');
+          $list.append([$form]);
+          $('#form').append($list)
+        }
+      } 
+    },
+    error: function(err){
+      throw err;
+    } 
+  })
+};
+
+// render form 
+
+
+// get fixtures 
+
+
+// render fixtures 
+
 
 
 
